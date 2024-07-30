7ca743a86a1183e4af922d1bdd9f88d78a120703
khanghou <khanghou.tan@sgtradextech.com>
2024-07-30 17:08:33 +0800

    fix: tests not being visible
    - refactor test file to match src dir structure

--- apps/w3c-cli/vitest.config.ts
+++ apps/w3c-cli/vitest.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     watch: false,
     globals: true,
     environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}', 'tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 
     reporters: ['default'],
     coverage: {
