32a6fdd8ad21bae401b49b991fb6bb2da9fba7b9
khanghou <khanghou.tan@sgtradextech.com>
2024-07-31 14:57:39 +0800

    refactor: cleanup generate code

--- apps/w3c-cli/src/commands/generate.test.ts
+++ apps/w3c-cli/src/commands/generate.test.ts
@@ -1,71 +1,55 @@
 import {
     GenerateKeyPairOptions,
     VerificationType,
-    generateKeyPair,
   } from '@tradetrust-tt/w3c-issuer';
-  import { execSync, spawn } from 'child_process';
-  import { beforeEach, describe, expect, it, vi } from 'vitest';
-  import stripAnsi from 'strip-ansi';
-  import inquirer from 'inquirer';
-  import fs from 'fs';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+import inquirer from 'inquirer';
+import fs from 'fs';
 import { generateAndSaveKeyPair, GenerateInput, promptQuestions } from './generate';
-  
-  
-  
-  vi.mock("inquirer")
-  vi.mock('fs');
-  
-  vi.mock('@tradetrust-tt/w3c-issuer', () => ({
-    generateKeyPair: vi.fn().mockResolvedValue({
-      publicKey: 'mockPublicKey',
-      privateKey: 'mockPrivateKey',
-    }),
-    VerificationType: {
-      Bls12381G2Key2020: 'Bls12381G2Key2020',
-      // Add other verification types if necessary
-    }
-  }));
-  
-  const mockSeed = 'FVj12jBiBUqYFaEUkTuwAD73p9Hx5NzCJBge74nTguQN';
-  const generateKeypairOption: GenerateKeyPairOptions = {
+
+vi.mock("inquirer")
+vi.mock('fs');
+
+// GLOBAL CONSTANTS
+const mockSeed = 'FVj12jBiBUqYFaEUkTuwAD73p9Hx5NzCJBge74nTguQN';
+const generateKeypairOption: GenerateKeyPairOptions = {
     type: VerificationType.Bls12381G2Key2020,
-  };
-  
-  describe('generate', () => {
-  
+};
+
+describe('generate', () => {
     beforeEach(() => {
-      vi.clearAllMocks();
+        vi.clearAllMocks();
     });
-  
+
     it('should prompt questions and obtain answers', async () => {
-  
-  
-      const input: GenerateInput = { 
-        encAlgo: generateKeypairOption.type, 
-        seedBase58: mockSeed, 
-        keyPath: '.'
-      };
-      // Automatically keys in "user input" that inquirer will receive
-      (inquirer.prompt as any).mockResolvedValue(input);
-  
-      const answers = await promptQuestions();
-  
-      expect(answers.encAlgo).toBe(generateKeypairOption.type);
-      expect(answers.seedBase58).toBe(mockSeed);
-      expect(answers.keyPath).toBe('.');
+        const input: GenerateInput = { 
+            encAlgo: generateKeypairOption.type, 
+            seedBase58: mockSeed, 
+            keyPath: '.'
+        };
+        // Automatically keys in "user input" that inquirer will receive
+        (inquirer.prompt as any).mockResolvedValue(input);
+
+        const answers = await promptQuestions();
+
+        expect(answers.encAlgo).toBe(generateKeypairOption.type);
+        expect(answers.seedBase58).toBe(mockSeed);
+        expect(answers.keyPath).toBe('.');
     });
-  
+
     it("should save keypair file", async () => {
-      const input: GenerateInput = { 
-        encAlgo: generateKeypairOption.type, 
-        seedBase58: mockSeed, 
-        keyPath: '.'
-      };
-      const writeFileMock = vi.spyOn(fs, 'writeFile').mockImplementation((path, data, callback) => {
-        callback(null);
-      });
-      await generateAndSaveKeyPair(input)
-  
-      expect(writeFileMock).toHaveBeenCalled()
+        const input: GenerateInput = { 
+            encAlgo: generateKeypairOption.type, 
+            seedBase58: mockSeed, 
+            keyPath: '.'
+        };
+
+        const writeFileMock = vi.spyOn(fs, 'writeFile').mockImplementation((path, data, callback) => {
+            callback(null);
+        });
+
+        await generateAndSaveKeyPair(input)
+
+        expect(writeFileMock).toHaveBeenCalled()
     })
-  });
+});
