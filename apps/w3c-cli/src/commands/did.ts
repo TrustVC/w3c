98cabff9288d0c639ea8e1d9186f3d6785c5c1de
khanghou <khanghou.tan@sgtradextech.com>
2024-08-01 14:29:34 +0800

    refactor: split generate-did function

--- apps/w3c-cli/src/commands/did.ts
+++ apps/w3c-cli/src/commands/did.ts
@@ -2,6 +2,7 @@ import inquirer from 'inquirer';
 import { Argv } from 'yargs';
 import fs from 'fs';
 import { issueDID, KeyPairType } from '@tradetrust-tt/w3c-issuer';
+import chalk from 'chalk';
 
 export const command = 'generate-did';
 
@@ -32,29 +33,47 @@ export const describe = 'Generate a new DID token file from a key pair file and 
 // export const builder = (yargs: Argv) => {};
 
 export const handler = async (argv: any) => {
-  // const answers = await inquirer.prompt(questions);
-  const { keyPairPath } = await inquirer.prompt(keypairQuestions);
-
-  let keypairData: KeyPairType;
   try {
-    const data = fs.readFileSync(keyPairPath, { encoding: 'utf8', flag: 'r' });
-    keypairData = JSON.parse(data);
+    const {keypairData, domainName, outputPath, } = await promptQuestions();
+      
+    // Issue the DID
+    const wellknown = await issueDID(keypairData);
+
+    // Write the wellknown data to a file
+    const wellknownPath = `${outputPath}/wellknown.json`;
+    writeFile(wellknownPath, wellknown);
   } catch (err) {
-    console.error(`Unable to find key pair file at ${keyPairPath}`);
-    console.error('Detailed error trace:', err);
-    return;
+    console.error(chalk.red('Error generating DID token:'), err);
   }
+};
 
-  const { domainName, outputPath } = await inquirer.prompt(questions);
-
-  keypairData.domain = domainName;
-  const wellknown = await issueDID(keypairData);
-  const wellknownPath = `${outputPath}/wellknown.json`;
-  fs.writeFile(wellknownPath, JSON.stringify(wellknown), (err) => {
+const writeFile = (path: string, data: any) => {
+  fs.writeFile(path, JSON.stringify(data), (err) => {
     if (err) {
-      console.error('Error writing file', err);
+      console.error(chalk.red('Error writing file'), err);
     } else {
-      console.log('File written successfully');
+      console.log(chalk.green('File written successfully to'), path);
     }
   });
+};
+
+
+
+const promptQuestions = async () => {
+  try {
+    // Prompt for the key pair path
+    const { keyPairPath } = await inquirer.prompt(keypairQuestions);
+
+    // Validate and read the key pair file
+    const data = fs.readFileSync(keyPairPath, { encoding: 'utf8', flag: 'r' });
+    const keypairData: KeyPairType = JSON.parse(data);
+
+    // Prompt for the domain name and output path
+    const { domainName, outputPath } = await inquirer.prompt(questions);
+
+    return { keypairData, domainName, outputPath };
+  } catch (err) {
+    console.error(chalk.red('Error during prompts or reading key pair file:'), err);
+    throw err; // Re-throw error to handle it in the main handler
+  }
 };
