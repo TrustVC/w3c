4629cc4ec9019c4df52761edc1da681b8b1b84f4
khanghou <khanghou.tan@sgtradextech.com>
2024-08-01 15:25:44 +0800

    refactor: organise tests

--- apps/w3c-cli/tests/commands/generate.test.ts
+++ apps/w3c-cli/tests/commands/generate.test.ts
@@ -2,7 +2,7 @@ import {
     GenerateKeyPairOptions,
     VerificationType,
   } from '@tradetrust-tt/w3c-issuer';
-import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { assert, beforeEach, describe, expect, it, vi } from 'vitest';
 import inquirer from 'inquirer';
 import fs from 'fs';
 import { generateAndSaveKeyPair, GenerateInput, promptQuestions } from '../../src/commands/generate';
@@ -21,35 +21,64 @@ describe('generate', () => {
         vi.clearAllMocks();
     });
 
-    it('should prompt questions and obtain answers', async () => {
-        const input: GenerateInput = { 
-            encAlgo: generateKeypairOption.type, 
-            seedBase58: mockSeed, 
-            keyPath: '.'
-        };
-        // Automatically keys in "user input" that inquirer will receive
-        (inquirer.prompt as any).mockResolvedValue(input);
-
-        const answers = await promptQuestions();
-
-        expect(answers.encAlgo).toBe(generateKeypairOption.type);
-        expect(answers.seedBase58).toBe(mockSeed);
-        expect(answers.keyPath).toBe('.');
-    });
-
-    it("should save keypair file", async () => {
-        const input: GenerateInput = { 
-            encAlgo: generateKeypairOption.type, 
-            seedBase58: mockSeed, 
-            keyPath: '.'
-        };
-
-        const writeFileMock = vi.spyOn(fs, 'writeFile').mockImplementation((path, data, callback) => {
-            callback(null);
+    describe("promptQuestions", () => {
+        it('should promptQuestions successfully', async () => {
+            const input: GenerateInput = { 
+                encAlgo: generateKeypairOption.type, 
+                seedBase58: mockSeed, 
+                keyPath: '.'
+            };
+            // Automatically keys in "user input" that inquirer will receive
+            (inquirer.prompt as any).mockResolvedValue(input);
+    
+            const answers = await promptQuestions();
+    
+            expect(answers.encAlgo).toBe(generateKeypairOption.type);
+            expect(answers.seedBase58).toBe(mockSeed);
+            expect(answers.keyPath).toBe('.');
         });
 
-        await generateAndSaveKeyPair(input)
-
-        expect(writeFileMock).toHaveBeenCalled()
     })
+
+    describe("generateAndSaveKeyPair", () => {
+        it("should successfully save keypair file", async () => {
+            const input: GenerateInput = { 
+                encAlgo: generateKeypairOption.type, 
+                seedBase58: '', 
+                keyPath: '.'
+            };
+    
+            const writeFileMock = vi.spyOn(fs, 'writeFile').mockImplementation((path, data, callback) => {
+                callback(null);
+            });
+    
+            await generateAndSaveKeyPair(input)
+
+            expect(writeFileMock).toHaveBeenCalledTimes(1)
+        })
+
+        // TODO: Add failure test case
+        // it('should fail to generateAndSaveKeyPair given invalid seed', async () => {
+        //     const input: GenerateInput = { 
+        //         encAlgo: generateKeypairOption.type, 
+        //         seedBase58: "ddd", 
+        //         keyPath: '.'
+        //     };
+        //     // Automatically keys in "user input" that inquirer will receive
+        //     (inquirer.prompt as any).mockResolvedValue(input);
+        //     const consoleLogSpy = vi.spyOn(console, 'log').mockImplementation(() => {})
+        //     const writeFileMock = vi.spyOn(fs, 'writeFile').mockImplementation((path, data, callback) => {
+        //         callback(null);
+        //     });
+    
+        //     await expect(generateAndSaveKeyPair(input)).rejects.toThrowError('Invalid seed');
+
+        //     // expect(consoleLogSpy).toHaveBeenCalledWith("Generating keys from provided seed...");
+
+        //     // expect(writeFileMock).toHaveBeenCalledTimes(0)
+
+        //     // assert.fail("Should throw an error")
+        // })
+    })
+
 });
