6f286fbd1b1c2e5d9a31c4cf7739bf08c31eb759
khanghou <khanghou.tan@sgtradextech.com>
2024-08-01 14:50:41 +0800

    feat: add did tests

---
+++ apps/w3c-cli/tests/commands/did.test.ts
@@ -0,0 +1,69 @@
+import {
+    GenerateKeyPairOptions,
+    VerificationType,
+  } from '@tradetrust-tt/w3c-issuer';
+import { assert, beforeEach, describe, expect, it, vi } from 'vitest';
+import inquirer from 'inquirer';
+import fs from 'fs';
+import { promptQuestions, saveIssuedDid } from '../../src/commands/did';
+import { IssueDidInput } from 'apps/w3c-cli/src/commands/did';
+
+vi.mock("inquirer")
+vi.mock('fs');
+
+const mockSeed = "9TuXkSMHnipzCMjgj56hft78LUqdGx8vbPazKDgRWfms";
+
+describe("did", () => {
+    beforeEach(() => {
+        vi.clearAllMocks();
+    })
+
+    it("should return correct answers for valid file path", async () => {
+        const input: IssueDidInput = {
+            keyPairPath: './keypair.json',
+            domainName: 'https://example.com',
+            outputPath: ''
+        };
+        const mockKeypairData = {};
+        (inquirer.prompt as any).mockResolvedValue(input);;
+        // Mocks the readFileSync function so that it successfully reads a file
+        (fs.readFileSync as any).mockReturnValue(JSON.stringify(mockKeypairData));
+        
+        const answers = await promptQuestions();
+
+        expect(answers.domainName).toBe(input.domainName);
+        expect(answers.outputPath).toBe(input.outputPath);
+
+        const expectedKeypairData = {
+            ...mockKeypairData,
+            domain: answers.domainName
+        }
+        expect(answers.keypairData).toStrictEqual(expectedKeypairData);
+    })
+
+    it("should return correct answers for invalid file path", async () => {
+        const input: IssueDidInput = {
+            keyPairPath: './keypair.json',
+            domainName: 'https://example.com',
+            outputPath: ''
+        };
+        (inquirer.prompt as any).mockResolvedValue(input);;
+        
+        try {
+            await promptQuestions();
+            assert.fail("Should throw an error")
+        } catch (err) {
+            expect(err).toBeInstanceOf(Error);
+        }
+    })
+
+    it("should write files successfully", async() => {
+        const writeFileMock = vi.spyOn(fs, 'writeFile').mockImplementation((path, data, callback) => {
+            callback(null);
+        });
+
+        await saveIssuedDid({}, {}, "./");
+
+        expect(writeFileMock).toHaveBeenCalledTimes(2);
+    })
+})
