4fcb737e35ac88ac92b92c53c30e98118596be52
rongquan1 <rongquan.low@gmail.com>
2024-08-08 17:59:53 +0800

    feat: w3c-vc sign and verify

--- packages/w3c-vc/src/lib/w3c-vc.test.ts
+++ packages/w3c-vc/src/lib/w3c-vc.test.ts
@@ -1,8 +1,51 @@
 import { describe, expect, it } from 'vitest';
-import { w3cVc } from './w3c-vc';
+import { signCredential, verifyCredential } from './w3c-vc';
 
-describe('w3cVc', () => {
-  it('should work', () => {
-    expect(w3cVc()).toEqual('w3c-vc');
+describe('Credential Signing and Verification', () => {
+  it('should successfully sign and verify a credential', async () => {
+    const credential = {
+      '@context': [
+        'https://www.w3.org/2018/credentials/v1',
+        'https://didrp-test.esatus.com/schemas/basic-did-lei-mapping/v1',
+        'https://w3id.org/security/bbs/v1',
+        'https://w3id.org/vc/status-list/2021/v1',
+      ],
+      credentialStatus: {
+        id: 'https://didrp-test.esatus.com/credentials/statuslist/1#27934',
+        statusListCredential: 'https://didrp-test.esatus.com/credentials/statuslist/1',
+        statusListIndex: 27934,
+        statusPurpose: 'revocation',
+        type: 'StatusList2021Entry',
+      },
+      credentialSubject: {
+        entityName: 'IMDA',
+        id: 'did:ethr:0x433097a1C1b8a3e9188d8C54eCC057B1D69f1638',
+        lei: '391200WCZAYD47QIKX37',
+        type: ['BasicDIDLEIMapping'],
+      },
+      issuanceDate: '2024-04-01T12:19:52Z',
+      expirationDate: '2029-12-03T12:19:52Z',
+      issuer: 'did:web:jocular-sunflower-144c0d.netlify.app',
+      type: ['VerifiableCredential'],
+    };
+
+    const signingKeyPair = {
+      id: 'did:web:jocular-sunflower-144c0d.netlify.app#keys-1',
+      controller: 'did:web:jocular-sunflower-144c0d.netlify.app',
+      privateKeyBase58: '44ToWFUFdm9eUa5So3fc1tCTpziiZLYM4qy5vZaGds1c',
+      publicKeyBase58:
+        't5Rdg91V9rVVKSGbeJ6ZJP32cED2Dad1nEjQpgMwMrmbvwryzgy8ppg9dwMNWJEKQL2dotEKAe1Z9iAe5e6wQBngCEqESavreSX8d1TfPtCyRYntYQe9pQWvaGae6NvJ68J',
+    };
+
+    // Sign the credential using the defined key pair
+    const signedCredential = await signCredential(credential, signingKeyPair);
+    expect(signedCredential).toBeDefined();
+
+    // Verify the signed credential
+    const verificationResult = await verifyCredential(signedCredential);
+    expect(verificationResult).toBeDefined();
+
+    // Check if the credential verification was successful
+    expect(verificationResult.verified).toBe(true);
   });
 });
--- packages/w3c-vc/src/lib/w3c-vc.ts
+++ packages/w3c-vc/src/lib/w3c-vc.ts
@@ -1,3 +1,92 @@
-export function w3cVc(): string {
-  return 'w3c-vc';
+import {
+  BbsBlsSignature2020,
+  Bls12381G2KeyPair,
+  KeyPairOptions,
+} from '@mattrglobal/jsonld-signatures-bbs';
+import jsonldSignatures from 'jsonld-signatures';
+
+// Define the type for the DID document
+interface DidDocument {
+  '@context'?: string[];
+  id?: string;
+  // Add other properties based on the structure of the DID document
 }
+
+// Define a type for the context loader function
+type DocumentLoader = (url: string) => Promise<{
+  contextUrl: string | null;
+  document: DidDocument;
+  documentUrl: string;
+}>;
+
+/**
+ * Creates and returns a custom document loader for JSON-LD contexts.
+ * The loader resolves DID URLs and fetches the corresponding DID documents.
+ * @returns {Promise<DocumentLoader>} A function that loads JSON-LD contexts.
+ */
+async function getDocumentLoader(): Promise<DocumentLoader> {
+  const customDocLoader = async (url: string) => {
+    if (url.includes('did:')) {
+      if (url.includes('#')) {
+        url = url.slice(0, url.indexOf('#'));
+      }
+      const id = url.split(':');
+      const path = id.map(decodeURIComponent).join('/') + '/.well-known/did.json';
+      url = path.replace('did/web/', 'https://');
+    }
+    try {
+      const results = await fetch(url, { redirect: 'follow' });
+
+      const resolveContext = {
+        contextUrl: null,
+        document: await results.json(),
+        documentUrl: results.url,
+      };
+
+      return resolveContext;
+    } catch (err) {
+      throw new Error();
+    }
+  };
+  return jsonldSignatures.extendContextLoader(customDocLoader);
+}
+
+/**
+ * Signs a credential using the BBS+ signature scheme.
+ * @param {Object} credential - The credential to be signed.
+ * @param {KeyPairOptions} keyPair - The key pair options for signing.
+ * @returns {Promise<Object>} The signed credential.
+ */
+export const signCredential = async (
+  credential: object,
+  keyPair: KeyPairOptions,
+): Promise<object> => {
+  const documentLoader = await getDocumentLoader();
+  const key = new Bls12381G2KeyPair(keyPair);
+
+  return await jsonldSignatures.sign(credential, {
+    suite: new BbsBlsSignature2020({ key }),
+    purpose: new jsonldSignatures.purposes.AssertionProofPurpose(),
+    documentLoader,
+  });
+};
+
+// Define the type for the verification result
+interface VerificationResult {
+  verified: boolean;
+}
+
+/**
+ * Verifies a credential using the BBS+ signature scheme.
+ * @param {Object} credential - The credential to be verified.
+ * @returns {Promise<VerificationResult>} The verification result.
+ */
+export const verifyCredential = async (credential: object): Promise<VerificationResult> => {
+  const documentLoader = await getDocumentLoader();
+
+  return await jsonldSignatures.verify(credential, {
+    suite: new BbsBlsSignature2020(),
+    purpose: new jsonldSignatures.purposes.AssertionProofPurpose(),
+    documentLoader,
+  });
+};
