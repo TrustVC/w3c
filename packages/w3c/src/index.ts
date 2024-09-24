8806062b7f83af9f8721e7a7dcc4b6f718109d74
nghaninn <43451336+nghaninn@users.noreply.github.com>
2024-09-24 17:52:00 +0800

    feat(w3c): add w3c package wrapper

---
+++ packages/w3c/src/index.ts
@@ -0,0 +1,4 @@
+export * as context from '@tradetrust-tt/w3c-context';
+export * as credentialStatus from '@tradetrust-tt/w3c-credential-status';
+export * as issuer from '@tradetrust-tt/w3c-issuer';
+export * as vc from '@tradetrust-tt/w3c-vc';
