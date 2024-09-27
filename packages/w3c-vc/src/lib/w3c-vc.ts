d03a844f8808102f0d54a70bee53b4f7674ec273
rongquan1 <rongquan.low@gmail.com>
2024-09-27 10:08:36 +0800

    feat: add transferable records context

--- packages/w3c-vc/src/lib/w3c-vc.ts
+++ packages/w3c-vc/src/lib/w3c-vc.ts
@@ -1,6 +1,6 @@
 import { KeyPairOptions } from '@mattrglobal/bls12381-key-pair';
 import { BbsBlsSignature2020, Bls12381G2KeyPair } from '@mattrglobal/jsonld-signatures-bbs';
-import { contexts, DID_V1_URL } from '@tradetrust-tt/w3c-context';
+import { contexts, trContexts, DID_V1_URL, TR_CONTEXT_URL } from '@tradetrust-tt/w3c-context';
 import { PrivateKeyPair } from '@tradetrust-tt/w3c-issuer';
 import { Resolver } from 'did-resolver';
 // @ts-ignore: No types available for jsonld-signatures
@@ -29,6 +29,12 @@ async function getDocumentLoader(): Promise<DocumentLoader> {
     contextUrl: null,
     document: contexts[DID_V1_URL],
     documentUrl: DID_V1_URL,
+  });
+
+  resultMap.set(TR_CONTEXT_URL, {
+    contextUrl: null,
+    document: trContexts[TR_CONTEXT_URL],
+    documentUrl: TR_CONTEXT_URL,
   });
 
   const resolveDid = async (did: string) => {
