01057c239b74688e7e091e247246e1aff6f9e236
nghaninn <43451336+nghaninn@users.noreply.github.com>
2024-10-08 23:49:55 +0800

    chore: fix script

--- scripts/setDependenciesForPublish.js
+++ scripts/setDependenciesForPublish.js
@@ -1,32 +1,29 @@
 const fs = require('fs');
-const glob = require('glob');
+const { glob } = require('glob');
 
 module.exports = async function () {
-  glob(__dirname + '/../' + '**/package.json', {}, (err, files) => {
-    files.forEach((file) => {
-      if (file.includes('node_modules')) {
+  const files = await glob(__dirname + '/../' + '**/package.json', { ignore: 'node_modules/**' });
+  files.forEach((file) => {
+    console.log('file', file);
+
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error('err', err);
         return;
       }
 
-      fs.readFile(file, 'utf8', (err, data) => {
+      const pkg = JSON.parse(data);
+      for (const key in pkg.dependencies) {
+        if (key.startsWith('@trustvc/')) {
+          pkg.dependencies[key] = '*';
+        }
+      }
+
+      fs.writeFile(file, JSON.stringify(pkg, null, 2), 'utf8', (err) => {
         if (err) {
-          console.error('err', err);
+          console.error(err);
           return;
         }
-
-        const pkg = JSON.parse(data);
-        for (const key in pkg.dependencies) {
-          if (key.startsWith('@trustvc/')) {
-            pkg.dependencies[key] = '*';
-          }
-        }
-
-        fs.writeFile(file, JSON.stringify(pkg, null, 2), 'utf8', (err) => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-        });
       });
     });
   });
