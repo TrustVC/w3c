2f913c5779a8a8ce31c12e22c0c677dac23fe94f
nghaninn <43451336+nghaninn@users.noreply.github.com>
2024-10-08 17:41:55 +0800

    chore: update release ci

---
+++ nxrelease.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  repositoryUrl: 'https://github.com/trustvc/w3c.git',
+  branches: [
+    {
+      name: 'main',
+      release: true,
+    },
+    {
+      name: 'alpha',
+      prerelease: 'alpha',
+    },
+    {
+      name: 'beta',
+      prerelease: 'beta',
+    },
+  ],
+  github: true,
+  changelog: true,
+  npm: true,
+  outputPath: '${PROJECT_DIR}/dist',
+  buildTarget: '${PROJECT_NAME}:build',
+  tagFormat: '${PROJECT_NAME}@${VERSION}',
+  debug: true,
+  plugins: [
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: true,
+        pkgRoot: '${PROJECT_DIR}',
+      },
+    ],
+  ],
+  releaseRules: [
+    {
+      subject: '*BREAKING CHANGE*',
+      release: 'major',
+    },
+    {
+      type: 'feat',
+      release: 'minor',
+    },
+    {
+      type: 'fix',
+      release: 'patch',
+    },
+    {
+      type: 'perf',
+      release: 'patch',
+    },
+  ],
+};
