7fe49f8e5a01cf36f4fdd87bccd72f87554a9072
Ng Han Inn <43451336+nghaninn@users.noreply.github.com>
2025-04-09 14:58:54 +0800

    fix: update fetchCredentialStatusVC to use documentLoader (#46)
    Co-authored-by: nghaninn <Ng Han Inn 43451336+nghaninn@users.noreply.github.com>

--- packages/w3c-vc/src/index.ts
+++ packages/w3c-vc/src/index.ts
@@ -1,12 +1,12 @@
 import { verifyCredentialStatus } from './lib/verify/credentialStatus';
 import {
   deriveCredential,
-  getDocumentLoader,
   isRawDocument,
   isSignedDocument,
   signCredential,
   verifyCredential,
 } from './lib/w3c-vc';
+import { getDocumentLoader } from '@trustvc/w3c-context';
 
 export * from './lib/types';
 export { CredentialStatusResult } from './lib/verify/credentialStatus/types';
