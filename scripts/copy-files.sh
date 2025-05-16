186ec7b6e27480d607a9001a0dabd3181a5c3fff
nghaninn <Ng Han Inn 43451336+nghaninn@users.noreply.github.com>
2025-05-16 15:49:03 +0800

    chore: attempt to resolve cors

--- scripts/copy-files.sh
+++ scripts/copy-files.sh
@@ -23,7 +23,7 @@ done
 echo "[[headers]]
   for = \"/*\"
   [headers.values]
-    Access-Control-Allow-Origin = \"*\"" > $DEST_DIR/netlify.toml
+    Access-Control-Allow-Origin = \"*\"" > ./public/netlify.toml
 
 # Optional: output a message when done
 echo "Files have been copied to $DEST_DIR"
