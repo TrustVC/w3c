729c10e14e047898efaf29cfb652f6187089b7d3
moiz-sgtradex <moiz.shaikh@sgtradextech.com>
2025-09-22 17:11:21 +0530

    feat: add resolve, resolveRepresentation and dereference functions

--- packages/w3c-issuer/src/did-web/index.ts
+++ packages/w3c-issuer/src/did-web/index.ts
@@ -1,7 +1,7 @@
 import { generateKeyPair } from './keyPair';
 import { issueDID } from './wellKnown';
-import { queryDidDocument } from './wellKnown/query';
+import { queryDidDocument, resolve, resolveRepresentation, dereference } from './wellKnown/query';
 
 export * from './keyPair/types';
 export * from './wellKnown/types';
-export { generateKeyPair, issueDID, queryDidDocument };
+export { generateKeyPair, issueDID, queryDidDocument, resolve, resolveRepresentation, dereference };
